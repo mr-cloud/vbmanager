69bc73005774278b33a03df27886367998f1cb24
mr-cloud <zhangxin1992pm@gmail.com>
2017-02-19 12:02:00 +0800

    create and config vm.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["VBMANAGER_URL_PREFIX"] = "/vbmanager"
 app.config["VBMANAGER_SITEURL"] = "http://localhost:8000"
 app.config["VBMANAGER_SITENAME"] = "My Site"
 
-vb = vbm(services)
+vb = vbm()
 
 
 @app.route("/")
@@ -18,4 +18,7 @@ def list_services():
 
 @app.route("/scale-out")
 def scale_out():
-    vb.create_config_VM()
+    return jsonify(vb.create_config_VM())
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000, use_reloader=True)
--- vbmanager.py
+++ vbmanager.py
@@ -1,42 +1,65 @@
 import virtualbox as vb
 import subprocess
+import time
 
 
 services = {
     1: "CREATE_VM",
     2: "DELETE_VM"
 }
+CLUSTER_MAX_SIZE = 7
 
 
-class VBManager():
-    def __init__(self, services):
+class VBManager:
+    def __init__(self):
         self.services = services
         self.vbox = vb.VirtualBox()
+        self.cold_vm_snapshot = 'scaling-in-vm-level'
 
     def list_all(self):
-        return self.services
+        return {'rst': 'success', 'data': self.services}
 
     def create_config_VM(self):
-        vms_name = [vm.name[len(vm.name)-1:] for vm in self.vbox.machines]
         isHotVM = False
         for vm in self.vbox.machines:
-            if vm.state == 1 and vm.name != 'hostname':
+            if vm.state == 1 and vm.name != 'namenode':
                 # start existed and power-offed VM.
-                completedProcess = subprocess.run("vboxmanage startvm {} --type headless".format(vm.name).split(), \
-                               stdout=subprocess.PIPE)
-                completedProcess.check_returncode()
+                try:
+                    completedProcess = subprocess.run("vboxmanage startvm {} --type headless".format(vm.name).split(), \
+                                   stdout=subprocess.PIPE, check=True)
+                except subprocess.CalledProcessError:
+                    print('Hot VM creation failed!')
+                    return {'rst': 'fail'}
                 isHotVM = True
                 break
         if not isHotVM:
+            if self.vbox.machines.__len__() >= CLUSTER_MAX_SIZE:
+                print('Cold VM creation failed: Beyond the cluster size limitation.')
+                return {'rst': 'fail'}
             # create a new VM by cloning.
-            completedProcess = subprocess.run(
-                "vboxmanage clonevm data1 --snapshot static-storm-cluster --name data{} --register"\
-                    .format(str(self.vbox.machines.__len__())), \
-                stdout=subprocess.PIPE)
+            try:
+                new_vm_name = 'data' + str(self.vbox.machines.__len__())
+                completedProcess = subprocess.run(
+                    "vboxmanage clonevm data1 --snapshot {} --name {} --register"\
+                        .format(self.cold_vm_snapshot, new_vm_name).split(), \
+                    stdout=subprocess.PIPE, check=True)
+            except subprocess.CalledProcessError:
+                print('Cold VM creation failed: Clone VM error.')
+                return {'rst': 'fail'}
+            try:
+                completedProcess = subprocess.run("vboxmanage startvm {} --type headless".format(new_vm_name).split(), \
+                                                  stdout=subprocess.PIPE, check=True)
+            except subprocess.CalledProcessError:
+                print('Cold VM creation failed: Start VM error.')
+                return {'rst': 'fail'}
             # config VM
+            vm = self.vbox.find_machine(new_vm_name)
+            while vm.state != 5:
+                time.sleep(5)
+            with vm.create_session() as session:
+                with session.console.guest.create_session('storm', '14641') as gs:
+                    process, stdout, stderr = gs.execute('sudo python3', ['/home/storm/lab/config_VM.py'])
 
-            completedProcess.check_returncode()
-
-
+        return {'rst': 'success'}
 
 
